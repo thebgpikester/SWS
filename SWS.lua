8747f64ccc50d15ecffbfb2767235c41dbc5749f
Mike <thebgpikester@hotmail.com>
2023-08-13 20:43:02 +0100

    Update SWS.lua license

--- SWS.lua
+++ SWS.lua
@@ -1,13 +1,29 @@
 --[[
-# README
+### License
+ This file is part of Simple Warehouse Saving.
+ Copyright (C) 2023 Michael Cole
 
-Simple Warehouse Saving by Pikey Aug 2023 version 1.0
+ Simple Warehouse Saving is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+# README 
+Simple Warehouse Saving by Pikey Aug 2023 version 1.0 (thebgpikester@hotmail.com)
 Thanks to Speed and Grimes for IntegratedbasicSerialize() function 
 Thanks to Eagle Dynamics for keeping the dream alive.
 
 This software is licensed under the Lesser GNU GPL 2007 
 Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
-Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.
+
 https://www.gnu.org/licenses/lgpl-3.0.txt
 This script is supplied with the full LICENSE at https://github.com/thebgpikester/SWS
 
