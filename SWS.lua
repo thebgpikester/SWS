f92b289645197810301ae82852df00cb064caf10
Mike <thebgpikester@hotmail.com>
2024-07-30 00:24:02 +0100

    Update SWS.lua

--- SWS.lua
+++ SWS.lua
@@ -37,21 +37,15 @@ Gets the contents of all DCS Airbase warehouses in the mission and saves them to
 ## USAGE
 Load this script at mission start as a DO SCRIPT.
 By default, DCS sets unlimited warehouses. Set some warehouses to have limited contents by clicking on them in the Mission Editor and unchecking 'Unlimited' next to any of aircraft, fuel and weapons. Set a fixed limit amount. Optionally, setup supply chains as per pg 104 DCS user manual EN 2020.pdf in your main DCS installation\Doc folder.
-
-## CONFIGURABLE ITEMS
-SWS.filepath = lfs.writedir().."SimpleWarehouse.lua"
-You can configure a custom directory for the save file here if needs be.
-
-SWS.updateDelaySeconds = 60
-You can change the timer interval for the saving of warehouses here. 
-
-SWS.filepath = lfs.writedir().."SimpleWarehouse.lua"
-You can configure a custom directory for the save file here if needs be.
 --]]
 
 SWS={} --don't touch
-SWS.filepath = lfs.writedir().."SimpleWarehouse.lua" --the file location is the root of your DCS instance Saved Games\DCS\ folder
-SWS.updateDelaySeconds = 60 --edit this, in testing there was no discernible impact.
+
+SWS.filepath = lfs.writedir().."SimpleWarehouse.lua"
+--You can configure a custom directory for the save file here if needs be.
+
+SWS.updateDelaySeconds = 60
+--You can change the timer interval for the saving of warehouses here. 
 
 --COMMON FUNCTIONS
  function SWS.IntegratedbasicSerialize(s)
